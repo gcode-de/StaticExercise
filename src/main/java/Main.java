f94367619325761b1cf6708c0892968003621fb5
Samuel Gesang <sgesang@mailo.com>
2024-04-24 15:34:41 +0200

    Add calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,27 @@ public class Main {
 
     public static void main(String[] args) {
 
+    //Static
     StaticExercise ex1 = new StaticExercise();
     StaticExercise ex2 = new StaticExercise();
 
     ex1.incrementInstanceCount();
     ex1.incrementInstanceCount();
-
     ex2.incrementInstanceCount();
+    StaticExercise.incrementTotalCount();
+    StaticExercise.incrementTotalCount();
+    StaticExercise.incrementTotalCount();
 
-    StaticExercise.incrementTotalCount();
-    StaticExercise.incrementTotalCount();
-    StaticExercise.incrementTotalCount();
-        System.out.println(ex1);
-        System.out.println(ex2);
+    System.out.println(ex1);
+    System.out.println(ex2);
+
+    //Calculator
+    System.out.println(Calculator.add(2,3));
+    System.out.println(Calculator.subtract(2,3));
+    System.out.println(Calculator.multiply(2,3));
+    System.out.println(Calculator.divide(6,2));
+
     }
+
+
 }
